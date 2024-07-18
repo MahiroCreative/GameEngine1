bfda36be9cdf13ed7f98f43ff1d753c2c64ad758
MahiroCreative <127273095+MahiroCreative@users.noreply.github.com>
2024-07-18 10:27:49 +0900

    up

--- 0.projects/unityGameEngineReUnity/Assets/Ge1_Unity2/Manager3.cs
+++ 0.projects/unityGameEngineReUnity/Assets/Ge1_Unity2/Manager3.cs
@@ -10,22 +10,19 @@ public class Manager3 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //ゲームオブジェクトの取得
         Force = GameObject.Find("Force");
         Impulse = GameObject.Find("Impulse");
         Acceleration = GameObject.Find("Acceleration");
         VelocityChange = GameObject.Find("VelocityChange");
+
+        //力の設定
         Pow = new Vector3(0,0,1.0f);
 
-        //
+        //力を加える
         Force.GetComponent<Rigidbody>().AddForce(Pow,ForceMode.Force);
         Impulse.GetComponent<Rigidbody>().AddForce(Pow,ForceMode.Impulse);
         Acceleration.GetComponent<Rigidbody>().AddForce(Pow, ForceMode.Acceleration);
         VelocityChange.GetComponent<Rigidbody>().AddForce(Pow, ForceMode.VelocityChange);
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        
     }
 }
